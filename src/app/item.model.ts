8729aa1d5b3795d69e16878a680c2e4a4f5fac5c
Kushboo Jain <kushbooj@thoughtworks.com>
2017-05-12 19:05:11 +0530

    Adding item as model

---
+++ src/app/item.model.ts
@@ -0,0 +1,11 @@
+export class Item {
+    name: String;
+    price: number;
+    quantity: number;
+    
+    constructor(name: String, price: number, quantity: number) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
--- src/app/itemslist/itemlist.component.ts
+++ src/app/itemslist/itemlist.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Item } from '../item.model';
 @Component({
     templateUrl: './itemlist.component.html',
 })
 
 export class ItemListComponent {
+    public items: Item[] = [new Item('Harry Potter part 1', 500, 10), new Item('Sherlock Holmes Part 1', 300, 5)];
 }
